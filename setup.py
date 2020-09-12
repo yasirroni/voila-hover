05c0f179c1e8f5a24b91db3c802561fb2a844e06
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2020-09-12 23:11:09 +0700

    edit setup.py to find jupyter

--- setup.py
+++ setup.py
@@ -1,5 +1,40 @@
 from setuptools import setup
+from setuptools.command.develop import develop
 import os
+import sys
+try:
+    import jupyter_core.paths as jupyter_core_paths
+except:
+    jupyter_core_paths = None
+
+pjoin = os.path.join
+
+class DevelopCmd(develop):
+    prefix_targets = [
+        ("nbconvert/templates", 'hover'),
+        ("voila/templates", 'hover')
+    ]
+    def run(self):
+        target_dir = os.path.join(sys.prefix, 'share', 'jupyter')
+        if '--user' in sys.prefix:  # TODO: is there a better way to find out?
+            target_dir = jupyter_core_paths.user_dir()
+        target_dir = os.path.join(target_dir)
+
+        for prefix_target, name in self.prefix_targets:
+            source = os.path.join('share', 'jupyter', prefix_target, name)
+            target = os.path.join(target_dir, prefix_target, name)
+            target_subdir = os.path.dirname(target)
+            if not os.path.exists(target_subdir):
+                os.makedirs(target_subdir)
+            rel_source = os.path.relpath(os.path.abspath(source), os.path.abspath(target_subdir))
+            try:
+                os.remove(target)
+            except:
+                pass
+            print(rel_source, '->', target)
+            os.symlink(rel_source, target)
+
+        super(DevelopCmd, self).run()
 
 data_files = []
 for root, dirs, files in os.walk('share'):
@@ -8,14 +43,17 @@ for root, dirs, files in os.walk('share'):
 
 setup_args = {
     'name': 'voila-hover',
-    'version': '0.0.1',
+    'version': '0.0.2',
     'packages': [],
     'data_files': data_files,
     'install_requires': [
         'voila>=0.1.6,<0.2'
     ],
     'author': 'yasirroni',
-    'url': 'https://github.com/yasirroni/voila-hover'
+    'url': 'https://github.com/yasirroni/voila-hover',
+    'cmdclass': {
+        'develop': DevelopCmd,
+    } if jupyter_core_paths else {},
 }
 
 if __name__ == '__main__':
